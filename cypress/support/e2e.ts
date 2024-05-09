003d46f1623a7a82971ecd8ebc089f2c1902fbc5
Nicholas <ni.wege@gmail.com>
2024-05-09 03:05:28 +0000

    cleanup: general documentation cleanup

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -20,6 +20,7 @@ import 'cy-mobile-commands';
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 Cypress.on('uncaught:exception', (err, runnable) => {
   // returning false here prevents Cypress from
   // failing the test
